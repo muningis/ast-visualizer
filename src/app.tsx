1abad9c952e4127c66c1f27094259d1d8dad8876
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-04-15 21:27:34 +0300

    ✨ Add list of some tools used

--- src/app.tsx
+++ src/app.tsx
@@ -10,10 +10,22 @@ function App() {
     console.log(program());
   })
   return (
-    <main class="grid grid-cols-2 w-screen h-screen">
-      <Graph program={program()} />
-      <Editor setProgram={setProgram} />
-    </main>
+    <div class="flex flex-col w-screen h-screen ">
+      <main class="grid grid-cols-2 flex-1 max-h-[calc(100%-2rem)]">
+        <Graph program={program()} />
+        <Editor setProgram={setProgram} />
+      </main>
+      <footer class="h-8 bg-slate-300 p-1">
+        Built with: 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://www.solidjs.com/">Solid</a>, 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://github.com/acornjs/acorn">Acorn</a>, 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://tailwindcss.com/">Tailwind</a>, 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://vitejs.dev/">Vite</a>, 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://bun.sh/">Bun</a>, 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="https://shiki.style/">Shiki</a> and 
+        <a class="underline" target="_blank" referrerpolicy="origin" href="">others</a>
+      </footer>
+    </div>
   );
 }
 
